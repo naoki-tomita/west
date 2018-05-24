9e52d80615ecb1c134a675612afc36d2dedc4c95
naoki-tomita <naoki.tomita@jrits.ricoh.co.jp>
2018-05-24 14:03:06 +0900

    Enable to work test.

---
+++ __tests__/index.test.ts
@@ -0,0 +1,28 @@
+import {
+  element,
+  setTimeout,
+  browser,
+} from "websocket-webdriver";
+
+describe("WebSocket-WebDriver", () => {
+  beforeEach(async () => {
+    jest.setTimeout(200000);
+    setTimeout(40000);
+    await browser.reload();
+  });
+
+  it("should test success.", async () => {
+    await element("#add").click();
+    expect(await element("ul > li:nth-child(1)").getText()).toBe(":0");
+  });
+
+  it("should test success.", async () => {
+    await element("#text").sendKeys("Hoge");
+    await element("#add").click();
+    expect(await element("ul > li:nth-child(1)").getText()).toBe("Hoge:0");
+    await element("#add").click();
+    await element("#add").click();
+    await element("#add").click();
+    expect(await element("ul > li:nth-child(4)").getText()).toBe(":3");
+  });
+});
